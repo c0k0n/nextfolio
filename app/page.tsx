032e363bddbf133e7d570bcb163910a6c436a5e5
c0k0n <88983407+c0k0n@users.noreply.github.com>
2024-11-01 20:07:06 +0800

    Change to Overview

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
           src="/profile.png"
           alt="Profile photo"
           className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
-          optimized="true"
+          unoptimized
           width={250}
           height={250}
           priority
